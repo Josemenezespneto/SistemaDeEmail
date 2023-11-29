34253c8618d122b9b5768b62d8cc125372244533
Josemenezespneto <menezespn@gmail.com>
2023-11-29 18:55:30 -0300

    001 - Criacao do pacote infrastructure para nosso AWS Ses, criacao da classe SesEmailSender e Adicao da notacao @service na classe EmailSenderService no pacote application

--- src/main/java/com/sistemaDeEmail/sistemaDeEmail/application/EmailSenderService.java
+++ src/main/java/com/sistemaDeEmail/sistemaDeEmail/application/EmailSenderService.java
@@ -3,7 +3,9 @@ package com.sistemaDeEmail.sistemaDeEmail.application;
 import com.sistemaDeEmail.sistemaDeEmail.adapters.EmailSenderGateway;
 import com.sistemaDeEmail.sistemaDeEmail.core.EmailSenderUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
@@ -14,6 +16,6 @@ public class EmailSenderService implements EmailSenderUseCase {
 
     @Override
     public void sendEmail(String to, String subject, String body) {
-
+        this.emailSenderGateway.senderEmail(to, subject, body);
     }
 }
